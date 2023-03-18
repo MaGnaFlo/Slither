1ec1fa5ccd518621be076abcebb03b808acde7d5
Florian Lardeux <magnaflo35@gmail.com>
2023-03-18 13:17:53 +0100

    added snake class and slither behavior

--- snake.py
+++ snake.py
@@ -5,28 +5,86 @@ from pygame.locals import QUIT
 
 WIDTH, HEIGHT = 640, 480 
 RED = (255,0,0)
+MOVE_LEFT = 0 
+MOVE_RIGHT = 1 
+MOVE_UP = 2 
+MOVE_DOWN = 3
 
 class SnakePart(pg.sprite.Sprite):
-	def __init__(self, width, height, color):
+	def __init__(self, pos, size, color):
 		super().__init__()
 
-		self.image = pg.Surface([width, height])
-		pg.draw.rect(self.image, color, pg.Rect(0,0,width, height))
+		self.image = pg.Surface(size)
+		pg.draw.rect(self.image, color, pg.Rect(*pos, *size))
 		self.rect = self.image.get_rect()
+		self.rect.x = 0
+		self.rect.y = 0
+
+	def set_pos(self, pos):
+		self.rect.x = pos[0]
+		self.rect.y = pos[1]
+
+	def get_pos(self):
+		return (self.rect.x, self.rect.y)
+
+
+class Snake(pg.sprite.Group):
+	def __init__(self):
+		super().__init__()
+
+		self._dpos = 20
+		self.pos_list = []
+
+	def add_part(self, part):
+		super().add(part)
+		self.pos_list.append(part.get_pos())
+
+	def update_pos(self, direction):
+		current_pos = (0,0)
+		for i, part in enumerate(self):
+			if i == 0:
+				current_pos = part.get_pos()
+				if direction == MOVE_LEFT:
+					part.set_pos((current_pos[0]-self._dpos, current_pos[1]))
+				if direction == MOVE_RIGHT:
+					part.set_pos((current_pos[0]+self._dpos, current_pos[1]))
+				if direction == MOVE_UP:
+					part.set_pos((current_pos[0], current_pos[1]-self._dpos))
+				if direction == MOVE_DOWN:
+					part.set_pos((current_pos[0], current_pos[1]+self._dpos))
+				
+			else:
+				temp = part.get_pos()
+				part.set_pos(current_pos)
+				current_pos = temp
+
 
 
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("The Snake")
+pg.display.set_caption("Slither")
 
-snake = pg.sprite.Group()
 
-part = SnakePart(20, 20, RED)
-part.rect.x = 200
-part.rect.y = 200
+# init the snake
+snake = Snake()
 
-snake.add(part)
+part = SnakePart((0,0), (20, 20), RED)
+part.set_pos((200,200))
+
+part2 = SnakePart((0,0), (20, 20), RED)
+part2.set_pos((220,200))
+
+part3 = SnakePart((0,0), (20, 20), RED)
+part3.set_pos((240,200))
+
+snake.add_part(part)
+snake.add_part(part2)
+snake.add_part(part3)
+
+
+
+
 
 
 clock = pg.time.Clock()
@@ -70,17 +128,14 @@ while running:
 				move_down = False
 				
 	if move_left:
-		for part in snake:
-			part.rect.x -= 20
+		snake.update_pos(MOVE_LEFT)
 	elif move_right:
-		for part in snake:
-			part.rect.x += 20
+		snake.update_pos(MOVE_RIGHT)
 	elif move_up:
-		for part in snake:
-			part.rect.y -= 20
+		snake.update_pos(MOVE_UP)
 	elif move_down:
-		for part in snake:
-			part.rect.y += 20
+		snake.update_pos(MOVE_DOWN)
+
 
 	screen.fill((0,0,0))
 	snake.update()
